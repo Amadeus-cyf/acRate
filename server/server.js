3031ff26384aa6972cbcba20e99ea0c6f34adff8
Yifan Cao <amadeuscao.cs@gmail.com>
2019-05-08 19:45:18 -0500

    add server base

---
+++ server/server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+const path = require('path');
+
+var app = express();
+//solve cross domain issue
+var allowCrossDomain =  function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, authorization");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+}
+
+app.use(express.static(__dirname + '/public'));
+app.use(allowCrossDomain);
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+require('./routes/')(app);
+require('./connection/connection.js')();
+
+
+var port = 4000;
+app.listen(port);
+console.log('Server running on port' + port);
