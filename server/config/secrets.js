d1427e1accc8a2db8974da837c9de8b60275429e
Yifan Cao <amadeuscao.cs@gmail.com>
2019-06-29 17:15:07 +0800

    move server to root

--- server/config/secrets.js
+++
@@ -1,7 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://animeScoringApp:animeScoringApp@cluster0-kvlze.mongodb.net/test?retryWrites=true",
-};
