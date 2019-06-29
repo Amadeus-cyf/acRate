57bca573ad1890c87930e5f0b0d552d7a40ebf3d
Yifan Cao <amadeuscao.cs@gmail.com>
2019-06-29 17:19:10 +0800

    add secrets

---
+++ config/secrets.js
@@ -0,0 +1,7 @@
+/*
+ * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
+ */
+module.exports = {
+    token : "secret-starter-mern",
+    mongo_connection : "mongodb+srv://animeScoringApp:animeScoringApp@cluster0-kvlze.mongodb.net/test?retryWrites=true",
+};
