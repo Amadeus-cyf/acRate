7307ba7bbbfcb3dcb607e5a6f8c70c72cb2711ca
Yifan Cao <amadeuscao.cs@gmail.com>
2019-06-29 18:01:52 +0800

    add heroku post build

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var express = require('express'),
 var app = express();
 var allowCrossDomain =  function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, authorization");
+    res.header("Access-Control-Allow-Headers", 
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, authorization");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
 }
@@ -22,6 +23,7 @@ app.use(allowCrossDomain);
 
 app.use(bodyParser.json({limit: '20mb'}));
 app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use(logger('dev'));
 app.use(cookieParser());
@@ -41,7 +43,6 @@ app.use(session({
 require('./config/passport');
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 require('./routes/')(app, router);
 
