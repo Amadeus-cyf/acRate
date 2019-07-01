68d4c95c07e3ecf42366582d4a0bd8cf5bfb3e5a
Yifan Cao <amadeuscao.cs@gmail.com>
2019-07-01 10:14:54 +0800

    fix detail bug

---
+++ config/secrets.js
@@ -0,0 +1,7 @@
+/*
+ * mongo connection
+ */
+module.exports = {
+    token : "secret-starter-mern",
+    mongo_connection : "mongodb+srv://animeScoringApp:animeScoringApp@cluster0-kvlze.mongodb.net/test?retryWrites=true",
+};
